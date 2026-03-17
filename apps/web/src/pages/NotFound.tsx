ac6fe3dada7cce135106293530ca3e6b12f1ddf7
Caio Pizzol <caio@harbourshare.com>
2026-03-17 11:16:39 -0300

    fix: sort imports in NotFound.tsx to pass biome lint

--- apps/web/src/pages/NotFound.tsx
+++ apps/web/src/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
 import { useDocumentTitle } from "../hooks/useDocumentTitle";
 
 export function NotFound() {
